ccb72e45222e387ec6cf00b8e8a4e6d734f89fbf
riteshshukla04 <riteshshukla2381@gmail.com>
2026-01-18 15:26:58 +0530

    Add github

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { EditorProvider } from './features/editor/context/EditorContext';
 import { EditorLayout } from './features/editor/EditorLayout';
+import { MobileWarning } from './components/MobileWarning';
 
 function App() {
   return (
     <EditorProvider>
       <EditorLayout />
+      <MobileWarning />
     </EditorProvider>
   )
 }
